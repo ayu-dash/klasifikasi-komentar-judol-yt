8b1bde969f3f788494c25537e092d25981723099
ayudash <adrianyudhaswara@gmail.com>
2025-12-29 23:22:56 +0700

    v.1.0.4

--- src/archive/tes.py
+++
@@ -1,5 +0,0 @@
-from autolabel import LabelingAgent, AutolabelDataset
-
-agent = LabelingAgent(config='config.json')
-ds = AutolabelDataset('comments_from_scraping_new.csv', config = config)
-agent.plan(ds)
